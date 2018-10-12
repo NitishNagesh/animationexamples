e3d5a0a903ce304c5404514793346cdd769d5b13
nitish nayak n <nageshnitishnnayak@gmail.com>
2018-10-12 10:42:58 +0530

    adding animation to the labels

--- animationexamples/ViewController.swift
+++ animationexamples/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController  , UITextFieldDelegate{
+   
+    @IBOutlet weak var helloworld: UILabel!
 
+    @IBOutlet weak var secondlabel: UILabel!
+    
+    @IBOutlet weak var hiddenLabel: UILabel!
+    
     @IBOutlet weak var pass: shakingcontroller!
     override func viewDidLoad() {
         super.viewDidLoad()
         pass.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
+     
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +29,46 @@ class ViewController: UIViewController  , UITextFieldDelegate{
     }
     func textFieldDidBeginEditing(_ textField: UITextField) {
         pass.shake()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        helloworld.center.y -= view.bounds.height
+        secondlabel.center.y += view.bounds.height
+        hiddenLabel.alpha = 0.0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 1.0, animations: {
+            self.helloworld.center.y += self.view.bounds.height
+        }, completion:nil)
+        UIView.animate(withDuration: 0.5, delay: 2.0, options: [ ], animations: {
+            self.view.backgroundColor = UIColor.blue
+        }, completion: nil)
+        secondanimation()
+     changeBackground()
+     UIView.animate(withDuration: 3.0, delay: 3.0, options: [] , animations:
+        {
+            
+    self.hiddenLabel.alpha = 1.0 }, completion: nil)
+    }
+    func changeBackground(){
+        UIView.animate(withDuration: 2.5,delay:3.0,options:[], animations: {
+            self.view.backgroundColor = UIColor.black
+            
+        }, completion: nil)
+        
+        
+        
+    }
+    func secondanimation(){
+        UIView.animate(withDuration: 2.0, animations: {
+            self.secondlabel.center.y -= self.view.bounds.height
+        }, completion:nil)
+        UIView.animate(withDuration: 3.0, delay: 1.0, options: [ ], animations: {
+            self.view.backgroundColor = UIColor.gray
+        }, completion: nil)
+        
     }
 
 
