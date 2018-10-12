e48bfa26b37d5335a5f74584147521e99f0dd1f0
nitish nayak n <nageshnitishnnayak@gmail.com>
2018-10-12 16:22:38 +0530

    with more number of animation example

--- animationexamples/ViewController.swift
+++ animationexamples/ViewController.swift
@@ -8,17 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController  , UITextFieldDelegate{
+class ViewController: UIViewController  , UITextFieldDelegate , CAAnimationDelegate{
    
     @IBOutlet weak var helloworld: UILabel!
 
+    @IBOutlet weak var `continue`: UIButton!
     @IBOutlet weak var secondlabel: UILabel!
     
     @IBOutlet weak var hiddenLabel: UILabel!
+      @IBOutlet var heightConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var pass: shakingcontroller!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         pass.delegate = self
      
     }
@@ -32,31 +35,64 @@ class ViewController: UIViewController  , UITextFieldDelegate{
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        self.continue.alpha = 0.0
+        
         helloworld.center.y -= view.bounds.height
+    
         secondlabel.center.y += view.bounds.height
         hiddenLabel.alpha = 0.0
+        pass.center.x -= view.bounds.width
+    
+    }
+    @IBAction func continueButtonPressed(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0.0,usingSpringWithDamping:0.2, initialSpringVelocity: 0.6,options: [ .curveEaseIn], animations: {
+            self.continue.bounds.size.width += 10
+        }, completion:{ finished in
+            UIView.animate(withDuration: 0.3, delay: 0.4,usingSpringWithDamping:0.2, initialSpringVelocity: 0.6,options: [ .curveEaseOut], animations: {
+                self.continue.bounds.size.width -= 10
+            }, completion:nil)
+            
+            })
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+             UIView.animate(withDuration:1, delay: 4, usingSpringWithDamping : 0.1,initialSpringVelocity: 0.5, options: [.curveEaseIn], animations: {
+                self.continue.alpha = 1.0
+            self.continue.center.y -= -30
+                self.continue.bounds.size.width += 10
+                UIView.animateKeyframes(withDuration: 1, delay: 0.2, options: [ ], animations: {
+                    
+                }, completion:nil)
+            
+        }, completion: nil)
+    
         UIView.animate(withDuration: 1.0, animations: {
             self.helloworld.center.y += self.view.bounds.height
-        }, completion:nil)
-        UIView.animate(withDuration: 0.5, delay: 2.0, options: [ ], animations: {
+        }, completion:{ finished in
+            self.secondanimation()
+        })
+        UIView.animate(withDuration: 0.5, delay: 0.7, options: [ ], animations: {
             self.view.backgroundColor = UIColor.blue
         }, completion: nil)
-        secondanimation()
-     changeBackground()
+    
+       
      UIView.animate(withDuration: 3.0, delay: 3.0, options: [] , animations:
         {
             
-    self.hiddenLabel.alpha = 1.0 }, completion: nil)
+    self.hiddenLabel.alpha = 1.0
+    self.hiddenLabel.alpha = 0.0
+     }, completion: nil)
+        
     }
     func changeBackground(){
-        UIView.animate(withDuration: 2.5,delay:3.0,options:[], animations: {
+        UIView.animate(withDuration: 1,delay:0.5,
+                       options:[], animations: {
             self.view.backgroundColor = UIColor.black
             
-        }, completion: nil)
+        }, completion: { finished in
+            self.animateTextField()
+        })
         
         
         
@@ -67,8 +103,23 @@ class ViewController: UIViewController  , UITextFieldDelegate{
         }, completion:nil)
         UIView.animate(withDuration: 3.0, delay: 1.0, options: [ ], animations: {
             self.view.backgroundColor = UIColor.gray
-        }, completion: nil)
+        }, completion: {finished in
+            self.changeBackground()
+        })
         
+    }
+    func animateTextField(){
+      
+        
+        UIView.animate(withDuration: 0.5, delay: 0.1, options: [], animations: {
+            self.pass.backgroundColor = UIColor.white
+        }, completion: nil)
+        UIView.animate(withDuration:1, delay: 4, usingSpringWithDamping : 0.1,initialSpringVelocity: 0.5, options: [.curveEaseIn], animations: {
+            self.pass.center.x += self.view.bounds.width
+        }, completion: nil)
+    }
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        self.view.backgroundColor = UIColor.green
     }
 
 
